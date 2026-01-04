3ea633902178bf16d53dc6fd7f4f677a2bddcf98
unknown <saadmansakib@iut-dhaka.edu>
2026-01-04 17:55:22 +0600

    fix : fix ECMAscript errors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,7 @@
-import express from "express"
-import cors from "cors"
+import app from "./app.js"
 
-const app = express()
+const PORT = process.env.PORT || 4000
 
-app.use(cors())
-app.use(express.json())
-
-app.get("/health", (_, res) => {
-    res.json({ status: "OK" })
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
-
-export default app
