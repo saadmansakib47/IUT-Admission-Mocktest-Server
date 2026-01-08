9833329fac44ecfaea49477bc9d6cb4f0f20fb9d
unknown <saadmansakib@iut-dhaka.edu>
2026-01-08 15:17:21 +0600

    feat : add controller logic for already submitted test, invalid test, and auto submit on timeout

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { startTest, submitTest } from "../services/test.service.js";
+import { TestSession } from "../models/TestSession.js";
 
 
 export const startTestHandler = async (req: Request, res: Response) => {
@@ -25,6 +26,27 @@ export const startTestHandler = async (req: Request, res: Response) => {
 
 export const submitTestHandler = async (req: Request, res: Response) => {
     const { testSessionId } = req.params;
+    const session = await TestSession.findById(testSessionId);
+
+    //Invalid test session
+    if (!session) {
+        return res.status(404).json({ message: "Test session not found" });
+    }
+
+
+    // Auto-submit on timeout
+    if (!session.submittedAt && session.endsAt && new Date() > session.endsAt) {
+        const score = await submitTest(testSessionId);
+        return res.json({ score, total: 100, autoSubmitted: true });
+    }
+
+    //Already submitted tests
+    if (session.submittedAt) {
+        return res.status(400).json({ message: "Test already submitted" });
+    }
+
+
     const score = await submitTest(testSessionId);
-    res.json({ score, total: 100 });
+    res.json({ score, total: 100, autoSubmitted: false });
 };
+
