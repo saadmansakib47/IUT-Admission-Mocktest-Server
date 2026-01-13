6a0e945855699f884a44d7b13de19a5d4fddd847
unknown <saadmansakib@iut-dhaka.edu>
2026-01-13 13:52:54 +0600

    fix : solve cast to objectId failure with static routes first, dynamic routes last

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -7,10 +7,12 @@ import { testReviewHandler } from "../controllers/testReview.controller.js";
 
 export const testRouter = Router();
 
-
+// Static and more specific routes first
 testRouter.post("/start", authMiddleware, startTestHandler);
+testRouter.get("/history", authMiddleware, testHistoryHandler);
+testRouter.get("/history/:testSessionId", authMiddleware, testReviewHandler);
+
+// Dynamic/Catch-all routes last
 testRouter.get("/:testSessionId", authMiddleware, getTestSessionHandler);
 testRouter.patch("/:testSessionId/answer", authMiddleware, answerQuestionHandler);
 testRouter.post("/:testSessionId/submit", authMiddleware, RateLimiter, submitTestHandler);
-testRouter.get("/history", authMiddleware, testHistoryHandler);
-testRouter.get("/history/:testSessionId", authMiddleware, testReviewHandler);
