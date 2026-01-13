db6b8b162ac613e785f5c03873c3b82876ea2f77
unknown <saadmansakib@iut-dhaka.edu>
2026-01-13 10:51:45 +0600

    feat : protect questionbank route

--- src/routes/questionBank.routes.ts
+++ src/routes/questionBank.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { fetchQuestionBanks } from "../controllers/questionBank.controller.js";
-
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 export const questionBankRouter = Router();
-questionBankRouter.get("/", fetchQuestionBanks);
+questionBankRouter.get("/", authMiddleware, fetchQuestionBanks);
