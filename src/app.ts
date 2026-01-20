8b2d603e35ebf52be3fa7228b320fdc9df57a7b6
unknown <saadmansakib@iut-dhaka.edu>
2026-01-20 12:53:00 +0600

    feat : admin can get all users list

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import { questionBankRouter } from "./routes/questionBank.routes.js";
 import adminQuestionRouter from "./routes/adminQuestion.routes.js";
 import adminContactRouter from "./routes/adminContact.routes.js";
 import adminStatsRouter from "./routes/adminStats.routes.js";
+import adminUserRouter from "./routes/adminUser.routes.js";
 import userRouter from "./routes/user.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import aiRoutes from "./routes/ai.routes.js";
@@ -39,6 +40,7 @@ app.use("/api/tests", testRouter);
 app.use("/api/admin/questions", adminQuestionRouter);
 app.use("/api/admin/contacts", adminContactRouter);
 app.use("/api/admin/stats", adminStatsRouter);
+app.use("/api/admin/users", adminUserRouter);
 app.use("/api/user", userRouter);
 app.use("/api/ai", aiRoutes);
 
